eea528ddc1c2632fe6f6305fbbe8bbf2ff29356f
JonathanHoffmann <jonathan.hoffmann@gmx.net>
2022-03-02 11:37:48 +0100

    V2
    click on item fram
    cost 3 levels

--- tpLodestone/src/main/java/teleportlodestone/tplodestone/lodestoneListener.java
+++ tpLodestone/src/main/java/teleportlodestone/tplodestone/lodestoneListener.java
@@ -3,43 +3,49 @@ package teleportlodestone.tplodestone;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.CompassMeta;
 
 public class lodestoneListener implements Listener {
 
     private Main main;
-    public lodestoneListener(Main main){ this.main = main;}
+
+    public lodestoneListener(Main main) {
+        this.main = main;
+    }
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent e){
+    public void onPlayerInteract(PlayerInteractEntityEvent e) {
         Player player = e.getPlayer();
-        if(e.getAction().equals(Action.RIGHT_CLICK_AIR)){
-            if(e.getHand().equals(EquipmentSlot.HAND)){
-                if(player.getItemInHand().getType().equals(Material.COMPASS)){
-                    ItemStack compass = player.getItemInHand();
-                    CompassMeta compassMeta = (CompassMeta) compass.getItemMeta();
-                    if(compassMeta.hasLodestone()){
-                        try{
+        Entity entity = e.getRightClicked();
+        if (entity instanceof ItemFrame) {
+            ItemStack item = ((ItemFrame) entity).getItem();
+            if (item.getType().equals(Material.COMPASS)) {
+
+                CompassMeta compassMeta = (CompassMeta) item.getItemMeta();
+                if (compassMeta.hasLodestone()) {
+                    if (player.getLevel() >= 3) {
+                        try {
+                            player.setLevel(player.getLevel() - 3);
                             Location location = compassMeta.getLodestone();
                             player.teleport(location);
                             player.sendMessage(ChatColor.YELLOW + "Player Teleported");
-                        }catch (NullPointerException exception){
+                            e.setCancelled(true);
+                        } catch (NullPointerException exception) {
                             exception.printStackTrace();
                             player.sendMessage(ChatColor.RED + "Teleportation Failed");
                         }
+                    } else {
+                        player.sendMessage(ChatColor.RED + "You need to pay 3 Levels to be teleported");
                     }
                 }
             }
         }
     }
-
 }
